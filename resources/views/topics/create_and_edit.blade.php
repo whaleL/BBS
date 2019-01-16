0739f331a30d47ce9040ed265145014de1fe6fb5
whalel <lixinger123@outlook.com>
2019-01-16 18:54:02 +0800

    添加编辑

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -35,10 +35,12 @@
 
               <div class="form-group">
                 <select class="form-control" name="category_id" required>
-                  <option value="" hidden disabled selected>请选择分类</option>
-                  @foreach ($categories as $value)
-                  <option value="{{ $value->id }}">{{ $value->name }}</option>
-                  @endforeach
+                  <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
+                    @foreach ($categories as $value)
+                      <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
+                        {{ $value->name }}
+                      </option>
+                    @endforeach
                 </select>
               </div>
 
@@ -58,7 +60,7 @@
 @endsection
 
 @section('styles')
-  <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+  <link type="text/css" href="{{ asset('css/simditor.css') }}" rel="stylesheet">
 @stop
 
 @section('scripts')
