3ed044a2cb500862b07a34626557b7743f274181
whalel <lixinger123@outlook.com>
2019-01-17 16:25:27 +0800

    测试回复功能

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -9,13 +9,16 @@ use App\Models\Reply;
 
 class ReplyObserver
 {
-    public function creating(Reply $reply)
-    {
-        //
-    }
 
-    public function updating(Reply $reply)
+
+
+	 public function created(Reply $reply)
     {
-        //
+        $reply->topic->reply_count = $reply->topic->replies->count();
+        $reply->topic->save();
+
+        // 通知话题作者有新的评论
+        $reply->topic->user->notify(new TopicReplied($reply));
     }
+   
 }
