5bc959f0329a4c3455bac21eb014374afb50c736
whalel <lixinger123@outlook.com>
2019-01-17 19:25:54 +0800

    添加插件

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -53,4 +53,9 @@ class Topic extends Model
         return $query->orderBy('created_at', 'desc');
     }
 
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
+
 }
